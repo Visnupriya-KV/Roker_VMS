28f32e090e36972439156ea6f11b66955bac12a5
Visnupriya-KV <vvenkateshwaran@metroploish.io>
2025-05-19 22:03:30 +0530

    added API_CreateUser_Test on dev

--- src/util.js
+++ src/util.js
@@ -1,6 +1,20 @@
 function generateRandomGroupName(prefix = 'TestGroup') {
-    const randomSuffix = Math.random().toString(36).substring(2, 8);
-    return `${prefix}_${randomSuffix}`;
-  }
+  const randomSuffix = Math.random().toString(36).substring(2, 8);
+  return `${prefix}_${randomSuffix}`;
+}
 
-  module.exports = {generateRandomGroupName}
+function generateRandomName(prefix = 'name', length = 6) {
+  const randomPart = Math.random().toString(36).substring(2, 2 + length);
+  return `${prefix}_${randomPart}`;
+}
+
+function generateRandomEmail(prefix = 'user', domain = 'yopmail.com') {
+  const randomPart = Math.random().toString(36).substring(2, 8);
+  return `${prefix}_${randomPart}@${domain}`;
+}
+
+module.exports = {
+  generateRandomGroupName,
+  generateRandomName,
+  generateRandomEmail
+};
